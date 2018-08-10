8b604ac18caea1db62b39311b602e10fec9e3645
Ahmed Sabry <ahmedsabry19@outlook.com>
2018-08-11 00:14:16 +0200

    PreGame jump animation

---
+++ Assets/Scripts/PreGamePirate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreGamePirate : MonoBehaviour
+{
+    public Animator animator;
+
+    private void OnMouseDown()
+    {
+        animator.SetTrigger("Jump");
+        GlobalCoroutineHandler.Instance.PlayCoroutineWithTime(() => SceneManager.LoadSceneAsync("2 - Game"), 3);
+    }
+}
